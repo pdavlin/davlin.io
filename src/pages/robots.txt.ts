a32168f7ada825f6e78d5c876895a542210a0aeb
Patrick Davlin <patrick.davlin@sharplink.com>
2024-06-17 17:23:59 -0500

    test netlify env vars

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,7 +1,9 @@
+const authToken = import.meta.env.PUBLIC_DARK_VISITOR_AUTH_TOKEN;
+
 const robotsTXT = await fetch("https://api.darkvisitors.com/robots-txts", {
   method: "POST",
   headers: {
-    Authorization: "Bearer " + import.meta.env.PUBLIC_DARK_VISITOR_AUTH_TOKEN,
+    Authorization: "Bearer " + authToken,
     "Content-Type": "application/json",
   },
   body: '{"agent_types":["AI Data Scraper","Undocumented AI Agent"],"disallow":"/"}',
