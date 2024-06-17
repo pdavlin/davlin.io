e458027cf9b13a31b0dfb48414c9576b5e7e98fe
Patrick Davlin <patrick.davlin@sharplink.com>
2024-06-17 12:29:54 -0500

    add auth token for api

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,7 +1,7 @@
 const robotsTXT = await fetch("https://api.darkvisitors.com/robots-txts", {
   method: "POST",
   headers: {
-    Authorization: "Bearer " + "a22ae245-1006-4025-9ad8-37d4c37f299b",
+    Authorization: "Bearer " + import.meta.env.PUBLIC_DARK_VISITOR_AUTH_TOKEN,
     "Content-Type": "application/json",
   },
   body: '{"agent_types":["AI Data Scraper","Undocumented AI Agent"],"disallow":"/"}',
@@ -9,7 +9,6 @@ const robotsTXT = await fetch("https://api.darkvisitors.com/robots-txts", {
 
 // Cache and serve this from your website's /robots.txt path
 export async function GET() {
-  console.log("trying to parse this response");
   return new Response(await robotsTXT.text(), {
     headers: { "Content-Type": "text/plain" },
     status: 200, // optional, default is 200
