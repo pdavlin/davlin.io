222645a2afb2244a5978174d6c27dc13fb7e7baf
Patrick Davlin <patrick.davlin@sharplink.com>
2024-06-17 17:43:40 -0500

    remove netlify integraion, back to SSG

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,19 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
-import netlify from "@astrojs/netlify";
-
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
   integrations: [sitemap()],
   site: "https://davlin.io",
   markdown: {
     shikiConfig: {
       themes: {
         dark: "github-light",
-        light: "github-dark"
+        light: "github-dark",
       },
       langs: [],
       // Enable word wrap to prevent horizontal scrolling
-      wrap: true
-    }
+      wrap: true,
+    },
   },
-  adapter: netlify()
 });
